ff4fd16238a1d9c55891ecc197e65188d1f35033
jiunkoo <jiunkoo0106@gmail.com>
2023-12-21 09:44:10 +0900

    feat: 텅 빈 servlet container 띄우기

--- src/main/java/tobyspring/hello/HelloApplication.java
+++ src/main/java/tobyspring/hello/HelloApplication.java
@@ -1,8 +1,15 @@
 package tobyspring.hello;
 
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+
 public class HelloApplication {
 
 	public static void main(String[] args) {
 
+		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+		WebServer webServer = serverFactory.getWebServer();
+		webServer.start();
 	}
 }
