67223ca5868ec0eb41c666c478df55236bc07508
jiunkoo <jiunkoo0106@gmail.com>
2023-12-21 10:08:58 +0900

    feat: 프론트 컨트롤러로 전환

--- src/main/java/tobyspring/hello/HelloApplication.java
+++ src/main/java/tobyspring/hello/HelloApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -20,16 +21,21 @@ public class HelloApplication {
 
 		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 		WebServer webServer = serverFactory.getWebServer(servletContext -> {
-			servletContext.addServlet("hello", new HttpServlet() {
+			servletContext.addServlet("frontcontroller", new HttpServlet() {
 				@Override
 				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-					String name = req.getParameter("name");
+					// 인증, 보안, 다국어, 공통 가능
+					if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+						String name = req.getParameter("name");
 
-					resp.setStatus(HttpStatus.OK.value());
-					resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-					resp.getWriter().println("Hello " + name);
+						resp.setStatus(HttpStatus.OK.value());
+						resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+						resp.getWriter().println("Hello " + name);
+					} else {
+						resp.setStatus(HttpStatus.NOT_FOUND.value());
+					}
 				}
-			}).addMapping("/hello");
+			}).addMapping("/*");
 		});
 		webServer.start();
 	}
