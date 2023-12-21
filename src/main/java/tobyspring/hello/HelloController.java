f36a4250d210fda594c068a9de5f1f7e8afde74e
jiunkoo <jiunkoo0106@gmail.com>
2023-12-21 21:02:57 +0900

    feat: 어노테이션 매핑 정보 사용

--- src/main/java/tobyspring/hello/HelloController.java
+++ src/main/java/tobyspring/hello/HelloController.java
@@ -2,6 +2,13 @@ package tobyspring.hello;
 
 import java.util.Objects;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/hello")
 public class HelloController {
 
 	private final HelloService helloService;
@@ -10,6 +17,8 @@ public class HelloController {
 		this.helloService = helloService;
 	}
 
+	@GetMapping
+	@ResponseBody
 	public String hello(String name) {
 
 		return helloService.sayHello(Objects.requireNonNull(name));
