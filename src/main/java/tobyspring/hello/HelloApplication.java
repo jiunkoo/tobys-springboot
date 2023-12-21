1d05da171decfb7a0c382706eab386b0176bc076
jiunkoo <jiunkoo0106@gmail.com>
2023-12-21 22:10:54 +0900

    feat: MySpringApplication

--- src/main/java/tobyspring/hello/HelloApplication.java
+++ src/main/java/tobyspring/hello/HelloApplication.java
@@ -1,12 +1,10 @@
 package tobyspring.hello;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @Configuration
@@ -27,24 +25,6 @@ public class HelloApplication {
 
 	public static void main(String[] args) {
 
-		AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
-
-			@Override
-			protected void onRefresh() {
-				super.onRefresh();
-
-				ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
-				DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
-
-				WebServer webServer = serverFactory.getWebServer(servletContext -> {
-
-					servletContext.addServlet("dispatcherServlet", dispatcherServlet)
-						.addMapping("/*");
-				});
-				webServer.start();
-			}
-		};
-		applicationContext.register(HelloApplication.class);
-		applicationContext.refresh();
+		MySpringApplication.run(HelloApplication.class, args);
 	}
 }
