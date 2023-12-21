319c09d990e50c327e6b95ec79724b493261e28a
jiunkoo <jiunkoo0106@gmail.com>
2023-12-21 21:09:58 +0900

    feat: 스프링 컨테이너로 통합

--- src/main/java/tobyspring/hello/HelloApplication.java
+++ src/main/java/tobyspring/hello/HelloApplication.java
@@ -10,18 +10,23 @@ public class HelloApplication {
 
 	public static void main(String[] args) {
 
-		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
+		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+			@Override
+			protected void onRefresh() {
+				super.onRefresh();
+
+				ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+				WebServer webServer = serverFactory.getWebServer(servletContext -> {
+
+					servletContext.addServlet("dispatcherServlet",
+						new DispatcherServlet(this)
+					).addMapping("/*");
+				});
+				webServer.start();
+			}
+		};
 		applicationContext.registerBean(HelloController.class);
 		applicationContext.registerBean(SimpleHelloService.class);
 		applicationContext.refresh();
-
-		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-		WebServer webServer = serverFactory.getWebServer(servletContext -> {
-
-			servletContext.addServlet("dispatcherServlet",
-				new DispatcherServlet(applicationContext)
-			).addMapping("/*");
-		});
-		webServer.start();
 	}
 }
