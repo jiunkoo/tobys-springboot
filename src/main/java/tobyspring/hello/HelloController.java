2879cf3276189b5ee6b4f8d82a5fd0dea9d471b3
jiunkoo <jiunkoo0106@gmail.com>
2023-12-23 07:57:18 +0900

    feat: Hello 컨트롤러에서 ApplicationContext 제거

--- src/main/java/tobyspring/hello/HelloController.java
+++ src/main/java/tobyspring/hello/HelloController.java
@@ -2,7 +2,6 @@ package tobyspring.hello;
 
 import java.util.Objects;
 
-import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,14 +12,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class HelloController {
 
 	private final HelloService helloService;
-	private final ApplicationContext applicationContext;
 
-	public HelloController(HelloService helloService, ApplicationContext applicationContext) {
+	public HelloController(HelloService helloService) {
 
 		this.helloService = helloService;
-		this.applicationContext = applicationContext;
-
-		System.out.println(applicationContext);
 	}
 
 	@GetMapping
