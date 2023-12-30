d989348fa4e1080e738a65498e964e6611a18bfc
jiunkoo <jiunkoo0106@gmail.com>
2023-12-30 18:41:17 +0900

    feat: Hello 레포지토리

--- src/test/java/tobyspring/hello/HelloApiTest.java
+++ src/test/java/tobyspring/hello/HelloApiTest.java
@@ -30,7 +30,7 @@ public class HelloApiTest {
 		TestRestTemplate rest = new TestRestTemplate();
 
 		ResponseEntity<String> res =
-			rest.getForEntity("http://localhost:9090//app/hello?name=", String.class);
+			rest.getForEntity("http://localhost:9090/app/hello?name=", String.class);
 
 		assertThat(res.getStatusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR);
 	}
